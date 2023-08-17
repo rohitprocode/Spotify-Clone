ffcedbe37f410b34b74e7e4f9cd45c38013f3c21
rohitprocode <rohit990797@gmail.com>
2023-08-17 18:53:38 +0530

    Seek Bar

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ console.log("Welcome to My Spotify Clone");
 
 //Initialize the variables
 let songIndex = 0;
-let song1 = new Audio('/Other Data/songs/1.mp3');
+let audioElement = new Audio('Other Data/songs/1.mp3');
 let masterPlay = document.getElementsByClassName('masterPlay')[0];
-let myProgressBar = document.getElementsByClassName('myProgressBar');
+let myProgressBar = document.getElementById('myProgressBar');
 let Dancing_bars = document.getElementById('dancing_bar');
+
 //Array of Object
 
 let songs = [
@@ -19,17 +20,25 @@ let songs = [
 
 
 // Handle play/pause event
-masterPlay.addEventListener('click', ()=> {
-    if (song1.paused|| (song1.currentTime <=0)){
-        song1.play();
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
         Dancing_bars.style.opacity = 1;
     }
     else {
-        song1.pause();
+        audioElement.pause();
         masterPlay.classList.add('fa-play-circle');
         masterPlay.classList.remove('fa-pause-circle');
         Dancing_bars.style.opacity = 0;
     }
+})
+
+
+//Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
 })
