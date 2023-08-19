a0a72a77a373da9fdccf667d19ece41f7b760338
rohitprocode <rohit990797@gmail.com>
2023-08-19 13:44:00 +0530

    Overwrite Another Songs Play

--- script.js
+++ script.js
@@ -10,25 +10,43 @@ let songItem = Array.from(document.getElementsByClassName('songItem'));
 //Array of Object
 
 let songs = [
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/1.mp3', coverPath: '/Other Data/covers/1.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/2.mp3', coverPath: '/Other Data/covers/2.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/3.mp3', coverPath: '/Other Data/covers/3.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/4.mp3', coverPath: '/Other Data/covers/4.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/5.mp3', coverPath: '/Other Data/covers/5.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/6.mp3', coverPath: '/Other Data/covers/6.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/7.mp3', coverPath: '/Other Data/covers/7.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/8.mp3', coverPath: '/Other Data/covers/8.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/9.mp3', coverPath: '/Other Data/covers/9.jpg' },
-    { songName: 'Tere waste falak se me', filtPath: '/Other Data/songs/10.mp3', coverPath: '/Other Data/covers/10.jpg'}
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/1.mp3', coverPath: '/Other Data/covers/1.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/2.mp3', coverPath: '/Other Data/covers/2.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/3.mp3', coverPath: '/Other Data/covers/3.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/4.mp3', coverPath: '/Other Data/covers/4.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/5.mp3', coverPath: '/Other Data/covers/5.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/6.mp3', coverPath: '/Other Data/covers/6.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/7.mp3', coverPath: '/Other Data/covers/7.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/8.mp3', coverPath: '/Other Data/covers/8.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/9.mp3', coverPath: '/Other Data/covers/9.jpg' },
+    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/10.mp3', coverPath: '/Other Data/covers/10.jpg'}
 ];
 
 songItem.forEach((element,i)=>{
-    console.log(element + " " + i)
     element.getElementsByTagName('img')[0].src = songs[i].coverPath;
+    element.getElementsByClassName('songName')[0].innerHTML= songs[i].songName;
 })
 
+//Stop Another Song Play
+const makeAllPlay = () =>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+// Play List Songs
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        makeAllPlay();
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+    })
+})
+
+
 // Handle play/pause event
-masterPlay.addEventListener('click', () => {
+masterPlay.addEventListener('click', ()=> {
     if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
