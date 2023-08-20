8edfae71430933048002727c7ea20d25da891f19
rohitprocode <rohit990797@gmail.com>
2023-08-20 14:57:29 +0530

    Previous and Next Button

--- script.js
+++ script.js
@@ -10,26 +10,26 @@ let songItem = Array.from(document.getElementsByClassName('songItem'));
 //Array of Object
 
 let songs = [
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/1.mp3', coverPath: '/Other Data/covers/1.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/2.mp3', coverPath: '/Other Data/covers/2.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/3.mp3', coverPath: '/Other Data/covers/3.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/4.mp3', coverPath: '/Other Data/covers/4.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/5.mp3', coverPath: '/Other Data/covers/5.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/6.mp3', coverPath: '/Other Data/covers/6.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/7.mp3', coverPath: '/Other Data/covers/7.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/8.mp3', coverPath: '/Other Data/covers/8.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/9.mp3', coverPath: '/Other Data/covers/9.jpg' },
-    { songName: 'Tere waste falak se me', filePath: '/Other Data/songs/10.mp3', coverPath: '/Other Data/covers/10.jpg'}
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/1.mp3', coverPath: '/Other Data/covers/1.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/2.mp3', coverPath: '/Other Data/covers/2.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/3.mp3', coverPath: '/Other Data/covers/3.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/4.mp3', coverPath: '/Other Data/covers/4.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/5.mp3', coverPath: '/Other Data/covers/5.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/6.mp3', coverPath: '/Other Data/covers/6.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/7.mp3', coverPath: '/Other Data/covers/7.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/8.mp3', coverPath: '/Other Data/covers/8.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/9.mp3', coverPath: '/Other Data/covers/9.jpg' },
+    { songName: 'Company - Emiway Bantai', filePath: '/Other Data/songs/10.mp3', coverPath: '/Other Data/covers/10.jpg' }
 ];
 
 //Display Cover Image and Song Name using Array Object
-songItem.forEach((element,i)=>{
+songItem.forEach((element, i) => {
     element.getElementsByTagName('img')[0].src = songs[i].coverPath;
-    element.getElementsByClassName('songName')[0].innerHTML= songs[i].songName;
+    element.getElementsByClassName('songName')[0].innerHTML = songs[i].songName;
 })
 
 // Handle play/pause event
-masterPlay.addEventListener('click', ()=> {
+masterPlay.addEventListener('click', () => {
     if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
@@ -46,21 +46,21 @@ masterPlay.addEventListener('click', ()=> {
 
 
 //Listen to Events Play / Pause
-audioElement.addEventListener('timeupdate', ()=>{ 
+audioElement.addEventListener('timeupdate', () => {
     // Update Seekbar
-   let progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    let progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
 })
 
 //Seekbar Action Event
-myProgressBar.addEventListener('change',()=>{
+myProgressBar.addEventListener('change', () => {
     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
 })
 
 
 //Stop Another Song Play
-const makeAllPlay = () =>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+const makeAllPlay = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
         element.classList.remove('fa-pause-circle');
         element.classList.add('fa-play-circle');
         masterPlay.classList.remove('fa-pause-circle');
@@ -69,18 +69,45 @@ const makeAllPlay = () =>{
 }
 
 // Play List Songs
-Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
         makeAllPlay();
         songIndex = parseInt(e.target.id);
-        console.log(songIndex);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        masterPlay.classList.add('fa-pause-circle');
-        masterPlay.classList.remove('fa-play-circle');
         audioElement.src = `/Other Data/songs/${songIndex + 1}.mp3`;
         audioElement.currentTime = 0;
-        audioElement.play();    
+        audioElement.play();
+        masterPlay.classList.add('fa-pause-circle');
+        masterPlay.classList.remove('fa-play-circle');
     })
 })
 
+
+//Play Previous 
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 9;
+    } else {
+        songIndex -= 1;
+    }
+    audioElement.src = `/Other Data/songs/${songIndex + 1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.add('fa-pause-circle');
+    masterPlay.classList.remove('fa-play-circle');
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 9) {
+        songIndex = 0;
+    }
+    else {
+        songIndex += 1;
+    }
+    audioElement.src = `/Other Data/songs/${songIndex + 1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.add('fa-pause-circle');
+    masterPlay.classList.remove('fa-play-circle');
+})
